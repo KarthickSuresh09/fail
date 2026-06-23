eb5c12ce9071f86a15752f26fa9172078a0c304e
karthick suresh <105416723+KarthickSuresh09@users.noreply.github.com>
2026-06-23 21:59:32 +0530

    Create iam.tf

---
+++ terraform/iam.tf
@@ -0,0 +1,20 @@
+//EKS Cluster Role
+resource "aws_iam_role" "eks_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
