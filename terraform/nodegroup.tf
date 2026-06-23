25db5617a8c7dbb935895ece713f31717664d0d5
karthick suresh <105416723+KarthickSuresh09@users.noreply.github.com>
2026-06-23 22:02:04 +0530

    Create nodegroup.tf

---
+++ terraform/nodegroup.tf
@@ -0,0 +1,26 @@
+resource "aws_eks_node_group" "worker_nodes" {
+
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "worker-group"
+
+  node_role_arn = aws_iam_role.node_role.arn
+
+  subnet_ids = [
+    aws_subnet.public1.id,
+    aws_subnet.public2.id
+  ]
+
+  scaling_config {
+    desired_size = 2
+    min_size     = 2
+    max_size     = 4
+  }
+
+  instance_types = ["t3.medium"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.worker1,
+    aws_iam_role_policy_attachment.worker2,
+    aws_iam_role_policy_attachment.worker3
+  ]
+}
